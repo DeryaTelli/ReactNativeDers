584d19bad955f05b7fb0574dd1d19ae706f70574
Mobilyst <2002derya2002@gmail.com>
2025-09-26 20:24:39 +0300

    learn image, components, props and activityIndicator

--- firstProject/App.js
+++ firstProject/App.js
@@ -5,20 +5,25 @@ import {
   View,
   TextInput,
   Pressable, //bunlar buton yapmamiza yariyor 
+  Image,
   
   } from 'react-native';
 
   import React , {useState} from 'react';
+  import Loading from './src/components/Loading';
 
 export default function App() {
 
  // const [state, setState]=useState("") // icine koydugum tirnakdan dolayi sadece string gelecegini anliyoruz 
-
-  const [name, setName]=useState("")
-  const [lastName, setLastName]=useState("")
+  
+ //local depolama farkli sayfaya gitigim zaman tutum yazilar kayip olacaktir 
+  const [email, setEmail]=useState("")
+  const [password, setPassword]=useState("")
   const [result, setResult]=useState("")
-  console.log(name)
-  console.log(lastName)
+  const [isLoading, setIsLoading]=useState(false)
+
+  // console.log(email)
+  // console.log(password)
 
   return (
     // <View style={styles.container}>
@@ -30,31 +35,45 @@ export default function App() {
     // </View>
 
     <View style={styles.container}>
-      <Text>Welcome{result}</Text>
-      <Text>Name</Text>
+
+      <Image 
+      source={require('./assets/images/icon.png')}
+      style={styles.imageStyle}
+      />
+      <Text style={styles.welcomeStyle}>Welcome {result}</Text>
+      <Text>Email</Text>
       <TextInput
-      placeholder='Enter your name'
+      placeholder='Enter Your Email'
       style={styles.textInputStyle}
-      onChangeText={setName}
-      value={name}
+      onChangeText={setEmail}
+      value={email}
+      inputMode='email' //klavyede @ .com gibi kisayollari getirir
       />
 
-      <Text> LastName</Text>
+      <Text> Password</Text>
       <TextInput
-      placeholder='Enter your last name'
+      placeholder='Enter Your Password'
       style={styles.textInputStyle}
-      onChangeText={setLastName}
-      value={lastName}
+      onChangeText={setPassword}
+      value={password}
+      secureTextEntry={true} //sifre girerken yildiz yapar
       />
 
       <Pressable
-      onPress={()=> setResult(name+" "+lastName)}
+      onPress ={()=>{setIsLoading(true)}}
+      // onPress={()=> setResult(name+" "+lastName)}
       style={({pressed})=> [{
         backgroundColor: pressed ? "gray" : "blue",
       },styles.buttonStyle]}
       >
         <Text style={styles.buttonTextStyle}>Save</Text>
       </Pressable>
+      {/* {isLoading ? <Loading/> : null}  eger true ise loading ol degisle bir sey yapma demek */}
+      {/* {isLoading && <Loading/>}  */}
+      {isLoading 
+        ? <Loading 
+        name="buttonName"
+        changeIsLoading={()=>setIsLoading(false)}/> : null}
     </View>
   );
 }
@@ -66,4 +85,34 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInputStyle:{
+    borderWidth:1,
+    width: '80%',
+    height: 50,
+    borderRadious: 10,
+    marginVertical:10,
+    textAlign:'center',
+    collor:'blue',
+    fontWeight:'bold',
+  },
+  buttonStyle:{
+    width:'80%',
+    height:50,
+    borderRadius:10,
+    alignItems:'center',
+    justifyContent:'center', 
+  },
+  buttonTextStyle:{
+    fontWeight:'bold',
+    color:'white',
+  },
+  imageStyle:{
+    width:100,
+    height:100,
+  },
+  welcomeStyle:{
+    fontSize:26,
+    fontWeight:'bold',
+  }
+
 });
---
+++ firstProject/src/components/Loading.js
@@ -0,0 +1,59 @@
+import { 
+    StyleSheet,
+    Text, 
+    View,
+    ActivityIndicator,
+    Pressable,
+    } from 'react-native'
+import React from 'react'
+
+// const Loading = (props) => { bu sekilde de kullanabilirsin 
+    const Loading = (name, changeIsLoading) => {
+    return (
+        <View style={styles.container}>
+            <Pressable
+            onPress={() => changeIsLoading()} 
+            style={[{}, styles.closeButtonContainer]}
+            ><Text style={styles.close}>X</Text></Pressable>
+            <ActivityIndicator size="large" color="#0000ff"/>
+            <Text style={styles.loginText}>{name} Loading..</Text>
+        </View>
+    )
+}
+
+export default Loading
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        position:'absolute',
+        width:'100%',
+        height:'100%',
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:'tomato',
+    },
+    loginText:{
+        fontWeight:'bold',
+        fontSize:16,
+        marginTop:10, //araya bosluk veriyoruz 
+        color:'white',
+    },
+    closeButtonContainer:{
+        backgroundColor: 'black',
+        width:30,
+        height:30,
+        borderRadius:50,
+        alignItems:'center',
+        justifyContent:'center',
+        position:'absolute',
+        top:50,
+        right:10,
+    },
+    close:{
+        color:'white',
+        fontWeight:'bold',
+        fontSize:16,
+    }
+
+})
