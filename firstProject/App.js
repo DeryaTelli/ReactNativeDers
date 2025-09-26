c635022e92f70b180797596d2d36e7be6f995eae
Mobilyst <2002derya2002@gmail.com>
2025-09-26 21:19:46 +0300

    learn native navigation

--- firstProject/App.js
+++ firstProject/App.js
@@ -1,118 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { 
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Pressable, //bunlar buton yapmamiza yariyor 
-  Image,
-  
-  } from 'react-native';
+import React from 'react'
+import SignUpPage from './src/screens/SignUpPage'
+import LoginPage from './src/screens/LoginPage'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-  import React , {useState} from 'react';
-  import Loading from './src/components/Loading';
-
-export default function App() {
-
- // const [state, setState]=useState("") // icine koydugum tirnakdan dolayi sadece string gelecegini anliyoruz 
-  
- //local depolama farkli sayfaya gitigim zaman tutum yazilar kayip olacaktir 
-  const [email, setEmail]=useState("")
-  const [password, setPassword]=useState("")
-  const [result, setResult]=useState("")
-  const [isLoading, setIsLoading]=useState(false)
-
-  // console.log(email)
-  // console.log(password)
-
+const Stack =createNativeStackNavigator();
+const App = () => {
   return (
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    //     <View style={styles.subContainer}>
-    //       <Text style={styles.textStyle}> Hello Derya</Text>
-    //     </View>
-    // </View>
-
-    <View style={styles.container}>
-
-      <Image 
-      source={require('./assets/images/icon.png')}
-      style={styles.imageStyle}
-      />
-      <Text style={styles.welcomeStyle}>Welcome {result}</Text>
-      <Text>Email</Text>
-      <TextInput
-      placeholder='Enter Your Email'
-      style={styles.textInputStyle}
-      onChangeText={setEmail}
-      value={email}
-      inputMode='email' //klavyede @ .com gibi kisayollari getirir
-      />
-
-      <Text> Password</Text>
-      <TextInput
-      placeholder='Enter Your Password'
-      style={styles.textInputStyle}
-      onChangeText={setPassword}
-      value={password}
-      secureTextEntry={true} //sifre girerken yildiz yapar
-      />
-
-      <Pressable
-      onPress ={()=>{setIsLoading(true)}}
-      // onPress={()=> setResult(name+" "+lastName)}
-      style={({pressed})=> [{
-        backgroundColor: pressed ? "gray" : "blue",
-      },styles.buttonStyle]}
-      >
-        <Text style={styles.buttonTextStyle}>Save</Text>
-      </Pressable>
-      {/* {isLoading ? <Loading/> : null}  eger true ise loading ol degisle bir sey yapma demek */}
-      {/* {isLoading && <Loading/>}  */}
-      {isLoading 
-        ? <Loading 
-        name="buttonName"
-        changeIsLoading={()=>setIsLoading(false)}/> : null}
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown:false}}>
+        {/* hangisi yukaridaysa sayfanin onu acilis sayfasi olarak algiliyor  */}
+        <Stack.Screen name='Login' component={LoginPage}/>
+        <Stack.Screen name='SignUp' component={SignUpPage}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  textInputStyle:{
-    borderWidth:1,
-    width: '80%',
-    height: 50,
-    borderRadious: 10,
-    marginVertical:10,
-    textAlign:'center',
-    collor:'blue',
-    fontWeight:'bold',
-  },
-  buttonStyle:{
-    width:'80%',
-    height:50,
-    borderRadius:10,
-    alignItems:'center',
-    justifyContent:'center', 
-  },
-  buttonTextStyle:{
-    fontWeight:'bold',
-    color:'white',
-  },
-  imageStyle:{
-    width:100,
-    height:100,
-  },
-  welcomeStyle:{
-    fontSize:26,
-    fontWeight:'bold',
-  }
+export default App
 
-});
---
+++ firstProject/src/screens/LoginPage.js
@@ -0,0 +1,136 @@
+    import { StatusBar } from 'expo-status-bar';
+    import { 
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    Pressable, //bunlar buton yapmamiza yariyor 
+    Image,
+    
+    } from 'react-native';
+
+    import React , {useState} from 'react';
+    import Loading from '../components/Loading';
+
+    const LoginPage = ({navigation})=> {
+
+    // const [state, setState]=useState("") // icine koydugum tirnakdan dolayi sadece string gelecegini anliyoruz 
+    
+    //local depolama farkli sayfaya gitigim zaman tutum yazilar kayip olacaktir 
+    const [email, setEmail]=useState("")
+    const [password, setPassword]=useState("")
+    const [result, setResult]=useState("")
+    const [isLoading, setIsLoading]=useState(false)
+
+    // console.log(email)
+    // console.log(password)
+
+    return (
+        // <View style={styles.container}>
+        //   <Text>Open up App.js to start working on your app!</Text>
+        //   <StatusBar style="auto" />
+        //     <View style={styles.subContainer}>
+        //       <Text style={styles.textStyle}> Hello Derya</Text>
+        //     </View>
+        // </View>
+
+        <View style={styles.container}>
+
+        <Image 
+        source={require('../../assets/images/icon.png')}
+        style={styles.imageStyle}
+        />
+        <Text style={styles.welcomeStyle}>Welcome {result}</Text>
+        <Text>Email</Text>
+        <TextInput
+        placeholder='Enter Your Email'
+        style={styles.textInputStyle}
+        onChangeText={setEmail}
+        value={email}
+        inputMode='email' //klavyede @ .com gibi kisayollari getirir
+        />
+
+        <Text> Password</Text>
+        <TextInput
+        placeholder='Enter Your Password'
+        style={styles.textInputStyle}
+        onChangeText={setPassword}
+        value={password}
+        secureTextEntry={true} //sifre girerken yildiz yapar
+        />
+
+        <Pressable
+        onPress ={()=>{setIsLoading(true)}}
+        // onPress={()=> setResult(name+" "+lastName)}
+        style={({pressed})=> [{
+            backgroundColor: pressed ? "gray" : "blue",
+        },styles.buttonStyle]}
+        >
+            <Text style={styles.buttonTextStyle}>Login</Text>
+        </Pressable>
+
+        <Pressable
+        onPress ={()=> navigation.navigate('SignUp')}
+        // onPress={()=> setResult(name+" "+lastName)}
+        style={({pressed})=> [{
+            backgroundColor: pressed ? "gray" : "lightblue",
+            marginTop:20,
+        },styles.signUpButtonStyle]}
+        >
+            <Text style={styles.buttonTextStyle}>Sign Up</Text>
+        </Pressable>
+        {/* {isLoading ? <Loading/> : null}  eger true ise loading ol degisle bir sey yapma demek */}
+        {/* {isLoading && <Loading/>}  */}
+        {isLoading 
+            ? <Loading 
+            name="buttonName"
+            changeIsLoading={()=>setIsLoading(false)}/> : null}
+        </View>
+    );
+    }
+    export default LoginPage;
+    const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textInputStyle:{
+        borderWidth:1,
+        width: '80%',
+        height: 50,
+        borderRadious: 10,
+        marginVertical:10,
+        textAlign:'center',
+        collor:'blue',
+        fontWeight:'bold',
+    },
+    buttonStyle:{
+        width:'80%',
+        height:50,
+        borderRadius:10,
+        alignItems:'center',
+        justifyContent:'center', 
+    },
+    buttonTextStyle:{
+        fontWeight:'bold',
+        color:'white',
+    },
+    imageStyle:{
+        width:100,
+        height:100,
+    },
+    welcomeStyle:{
+        fontSize:26,
+        fontWeight:'bold',
+    },
+    signUpButtonStyle:{
+        width:'30%',
+        height:40,
+        borderRadius:10,
+        alignItems:'center',
+        justifyContent:'center', 
+    }
+
+    });
