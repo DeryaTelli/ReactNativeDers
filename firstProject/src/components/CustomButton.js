23c387ad30405fbf8e0a2449bbd5e5ed24169faf
Mobilyst <2002derya2002@gmail.com>
2025-10-10 21:23:25 +0300

    hata duzeltilemedi animasyon yapimi ogrenilecek

--- firstProject/src/components/CustomButton.js
+++ firstProject/src/components/CustomButton.js
@@ -1,13 +1,11 @@
-    import { Animated as RNAnimated, StyleSheet, Text, View , Pressable, Animated} from 'react-native'
+    import { StyleSheet, Text, Pressable} from 'react-native'
     import React from 'react'
-import Animated, { FadeIn } from 'react-native-reanimated';
+    
     const CustomButton = ({buttonText, setWidth, handleOnPress, buttonColor, pressedButton}) => {
     
-    const  AnimatedPressable = Animated.createAnimatedComponent(Pressable);
-
+    
     return (
-        <AnimatedPressable
-            entering={FadeIn.duration(200)}
+        <Pressable
             onPress={()=>handleOnPress()}
             style={({pressed})=> [{
                     backgroundColor: pressed ? pressedButton : buttonColor,
@@ -15,7 +13,7 @@ import Animated, { FadeIn } from 'react-native-reanimated';
                 },styles.buttonStyle]}
                 >
                     <Text style={styles.buttonTextStyle}>{buttonText}</Text>
-            </AnimatedPressable>
+        </Pressable>
     )
     }
 
