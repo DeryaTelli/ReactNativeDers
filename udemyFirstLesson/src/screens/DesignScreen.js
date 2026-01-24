428167feaae19da7f4acb3d149cf5ab3ccb98346
Mobilyst <2002derya2002@gmail.com>
2026-01-24 21:37:30 +0300

    position hizalamasi

--- udemyFirstLesson/src/screens/DesignScreen.js
+++ udemyFirstLesson/src/screens/DesignScreen.js
@@ -5,8 +5,8 @@
     return (
         <View style={styles.mainView}>
             <Text style={styles.mainTextOne}>1</Text>
-            <Text style={styles.mainTextTwo}>2</Text>
-            <Text style={styles.mainTextThree}>3</Text>
+            {/* <Text style={styles.mainTextTwo}>2</Text>
+            <Text style={styles.mainTextThree}>3</Text> */}
             {/* <View
             style={{width:50, height:50, backgroundColor:'pink', }}
             />
@@ -24,8 +24,8 @@
         mainView:{
             borderWidth:3,
             borderColor:'pink',
-            margin:10,
-            borderRadius:10,
+            margin:20,
+            borderRadius:20,
             //alignItems:'stretch', hic bir sey degistirmeyen deger 
             //alignItems:'flex-start', // baslangica gelir deger  
             //flexDirection: 'row', // yada yada dikeyde hizalama nesneleri 
@@ -44,7 +44,13 @@
             //paddingHorizontal:20, // sadece x ekseninde icerde bosluk vericek
             //paddingVertical:20, // sadece y ekseninde icerde bosluk vericek
             borderRadius:10, // koseleri yuvarlatir
-            felx:4
+            //felx:4
+            //alignSelf:'flex-start'
+            position: 'absolute',
+            //top:0,
+            //left:0,
+            //bottom:0,
+            //right:0
         },
         mainTextTwo:{
             borderWidth:1,
@@ -52,8 +58,12 @@
             margin:10,
             padding:20,
             borderRadius:10,
-            flex:5 // bos alani kapliyor 
-            
+            //flex:5 // bos alani kapliyor 
+            //top:10, // yukaridan bosluk veriyor 
+            left:10,
+            //bottom:20 // asagidan bosluk vererek seklin yukari cikmasini saglicak 
+
+
         },
         mainTextThree:{
             borderWidth:1,
@@ -61,7 +71,7 @@
             margin:10,
             padding:20,
             borderRadius:10,
-            flex:1
+            //flex:1
             
         }
 
