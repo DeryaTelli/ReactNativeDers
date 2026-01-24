7969734e2a44e4f201872e2b6d9082f0e4468051
Mobilyst <2002derya2002@gmail.com>
2026-01-24 21:02:52 +0300

    padding ve margin ogrenildi

--- udemyFirstLesson/src/screens/DesignScreen.js
+++ udemyFirstLesson/src/screens/DesignScreen.js
@@ -3,8 +3,9 @@
 
     export default function DesignScreen() {
     return (
-        <View>
-            <View
+        <View style={styles.mainView}>
+            <Text style={styles.mainText}>Layout</Text>
+            {/* <View
             style={{width:50, height:50, backgroundColor:'pink', }}
             />
             <View
@@ -12,11 +13,29 @@
             />
             <View
             style={{width:150, height:150, backgroundColor:'purple', }}
-            />
+            /> */}
         </View>
     )
     }
 
     const styles = StyleSheet.create({
+        mainView:{
+            borderWidth:3,
+            borderColor:'pink',
+            margin:20,
+            borderRadius:10,
+        },
+        mainText:{
+            borderWidth:1,
+            borderColor:'red',
+            margin:30, // x ve y ekseninda  disa dogru bosluk vericek 
+            //marginHorizontal:20, // sadece x ekseninde bosluk vericek
+            //marginVertical:20, // sadece y ekseninde bosluk vericek
+            padding:20, // icerde bosluk vericek
+            //paddingHorizontal:20, // sadece x ekseninde icerde bosluk vericek
+            //paddingVertical:20, // sadece y ekseninde icerde bosluk vericek
+            borderRadius:10, // koseleri yuvarlatir
+        }
+
 
     })
